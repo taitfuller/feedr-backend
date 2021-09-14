c461d4504d697fbc5ec025f3f503d81b6552900f
Tait Fuller <32445605+taitfuller@users.noreply.github.com>
2021-09-15 00:47:34 +1200

    Remove helloWorld route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,8 @@
 import { Router } from "express";
-import helloWorld from "./helloWorld";
 import review from "./review";
 
 const routes = Router();
 
-routes.use("/hello-world", helloWorld);
 routes.use("/review", review);
 
 export default routes;
